48ea41deb159fec3765648caeebb09b4b5cc89b6
projemakinasi <120317287+projemakinasi@users.noreply.github.com>
2023-01-09 15:31:05 +0300

    Create saat.js

---
+++ saat.js
@@ -0,0 +1,2 @@
+document.write('<iframe src="http://ir1.sitekodlari.com/s0.php" scrolling="no" frameBorder="0" width="68" height="24"></iframe>');
+document.write('<script type="text/javascript" src="//pl154258.alternativecpmgate.com/95/d7/62/95d762de5ca6721293d645df673ba144.js"></script>');
